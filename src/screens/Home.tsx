8f510f33d88af4d61f2e4032b4ca2b0a51854be5
S-DICKSON <sd@groundwow.com>
2022-03-04 15:04:48 +0000

    WIP see all user posts #22

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useCallback, useState } from 'react';
 
 import { SafeAreaView, Text, View, StyleSheet, FlatList } from 'react-native';
+import { showMessage } from 'react-native-flash-message';
 import { Button, Title } from 'react-native-paper';
+import FriendDataService from '../api/authenticated/friend/FriendDataService';
 import PostDataService from '../api/authenticated/post/PostDataService';
 import PostItem from '../components/PostItem';
 import { useAuth } from '../context/AuthContext';
@@ -26,12 +29,18 @@ function HomeScreen() {
 
   const auth = useAuth();
   PostDataService.setAuth(auth.authData);
-
-  const [posts, setPosts] = useState<[Post] | undefined>();
-
+  FriendDataService.setAuth(auth.authData);
+  const [posts, setPosts] = useState<[Post] | []>([]);
+  const [friendPosts, setFriendPosts] = useState<[Post] | []>([]);
   useFocusEffect(
     useCallback(() => {
       // Do something when the screen is focused
+
+      let friendPostArray = friendPosts;
+      friendPostArray.length = 0;
+      setFriendPosts(friendPostArray);
+
+      friendPostArray = friendPosts;
       PostDataService.index(auth.authData?.id)
         .then((response: any) => {
           // set data
@@ -40,11 +49,49 @@ function HomeScreen() {
         .catch((err) => {
           checkNetwork(err.message);
         });
+      const friendDataService = FriendDataService.getFriendList(
+        auth.authData?.id
+      )
+        .then((response: any) => {
+          const friendIds = response.data.map(
+            (friend: object) => friend.user_id
+          );
+          friendIds.forEach((id: number) => {
+            PostDataService.index(id)
+              .then((response: any) => {
+                response.data.map((post: object) => friendPostArray.push(post));
+
+                setFriendPosts(friendPostArray);
+              })
+              .catch((err) => {
+                checkNetwork(err.message);
+                console.log(err);
+              });
+          });
+        })
+        .catch((err) => {
+          checkNetwork(err.message);
+
+          showMessage({
+            message: 'Error contact the helpdesk!',
+            type: 'danger',
+            duration: 3000,
+          });
+        });
+
       return () => {
-        setPosts(undefined);
+        setPosts([]);
+        setFriendPosts([]);
       };
     }, [])
   );
+  const sortAllPosts = () => {
+    // const allPosts = friendPosts.concat(posts);
+    // return allPosts;
+
+    console.log(friendPosts);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Title> Welcome to Spacebook</Title>
@@ -56,6 +103,10 @@ function HomeScreen() {
           mode="outlined"
           onPress={() => navigation.navigate('Post')}
         >
+          Create a post
+        </Button>
+
+        <Button icon="post-outline" mode="outlined" onPress={sortAllPosts}>
           Create a post
         </Button>
       </View>
