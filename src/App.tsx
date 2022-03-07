135ae274b86e856ce1ff3cdb88fa1af6db084446
S-DICKSON <sd@groundwow.com>
2022-03-07 16:43:16 +0000

    WIP Creating scheduler #26

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,61 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
+import BackgroundFetch from 'react-native-background-fetch';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Router } from './navigation/Router';
 import { AuthProvider } from './context/AuthContext';
 
-function App() {
-  return (
-    <AuthProvider>
-      <PaperProvider>
-        <Router />
-      </PaperProvider>
-    </AuthProvider>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    // Initialize BackgroundFetch ONLY ONCE when component mounts.
+    this.initBackgroundFetch();
+  }
+
+  async initBackgroundFetch() {
+    // BackgroundFetch event handler.
+    const onEvent = async (taskId) => {
+      console.log('[BackgroundFetch] task: ', taskId);
+      // Do your background work...
+      await this.addEvent(taskId);
+      // IMPORTANT:  You must signal to the OS that your task is complete.
+      BackgroundFetch.finish(taskId);
+    };
+
+    // Timeout callback is executed when your Task has exceeded its allowed running-time.
+    // You must stop what you're doing immediately BackgroundFetch.finish(taskId)
+    const onTimeout = async (taskId) => {
+      console.warn('[BackgroundFetch] TIMEOUT task: ', taskId);
+      BackgroundFetch.finish(taskId);
+    };
+
+    // Initialize BackgroundFetch only once when component mounts.
+    const status = await BackgroundFetch.configure(
+      { minimumFetchInterval: 15 },
+      onEvent,
+      onTimeout
+    );
+
+    console.log('[BackgroundFetch] configure status: ', status);
+  }
+
+  // Add a BackgroundFetch event to <FlatList>
+  async addEvent(taskId) {
+    console.log('doing task');
+    const postsFromStorage = await AsyncStorage.getItem('@Posts');
+    const data = JSON.parse(postsFromStorage);
+
+    console.log(data);
+  }
+
+  render() {
+    return (
+      <AuthProvider>
+        <PaperProvider>
+          <Router />
+        </PaperProvider>
+      </AuthProvider>
+    );
+  }
 }
 
 export default App;
