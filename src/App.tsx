2e95fb734f9f5bbaa33ee3acebe971696abc53eb
S-DICKSON <sd@groundwow.com>
2022-03-07 15:53:44 +0000

    Revert "Installed background task scheduler #26"
    This reverts commit 921f568cb1cfd0f4eef01266195437803a35b8ad.

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,14 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import { Router } from './navigation/Router';
 import { AuthProvider } from './context/AuthContext';
 
-class App extends React.Component {
-  componentDidMount() {
-    // Initialize BackgroundFetch ONLY ONCE when component mounts.
-    console.log('test');
-  }
-
-  render() {
-    return (
-      <AuthProvider>
-        <PaperProvider>
-          <Router />
-        </PaperProvider>
-      </AuthProvider>
-    );
-  }
+function App() {
+  return (
+    <AuthProvider>
+      <PaperProvider>
+        <Router />
+      </PaperProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
